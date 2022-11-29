8eef9f5faa2fca23fa4672b1a6840c343daa30ab
Nate Choiniere <choiniere@unc.edu>
2022-11-29 13:32:16 -0500

    a04

---
+++ server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const {roll} = require('./lib/roll.js');
+const minimist = require('minimist');
+const express = require('express');
+const app = express();
+
+var args = process.argv.slice(2);
+const port = args.port || 5000;
+
+app.get('/', (req, res, next) => { // endpoint
+	res.send('Hello, world!');
+})
+
+app.listen(port, () => {
+	console.log("Server listening on port " + port);
+})
