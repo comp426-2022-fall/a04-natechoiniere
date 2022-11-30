018757ff419a2cf92e2622a1577ee3aa491120d8
Nate Choiniere <choiniere@unc.edu>
2022-11-30 06:38:34 -0500

    a04

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 #!/usr/bin/env node
 // test
-const {roll} = require('./lib/roll.js');
-const minimist = require('minimist');
-const express = require('express');
+import {roll} from "./lib/roll.js";
+import minimist from "minimist";
+import express from "express";
 const app = express();
-
 var args = process.argv.slice(2);
-const port = args.port || 5000;
+var parsedArgs = minimist(args);
 
-app.get('/', (req, res, next) => { // endpoint
-	res.send('Hello, world!');
-})
+const port = parsedArgs.port || 5000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res, next) => { // endpoint is the first arg
+	res.status(404).send('404 NOT FOUND');
+	
+});
+app.get('/app/', (req, res, next) => {
+	res.status(200).send('200 OK');
+	
+});
+app.post('/app/roll/', (req, res, next) => {
+	// accept JSON or URLEncoded data body?
+	// req.body for the data body
+	// req.params for  everything before the ? in URL string
+	// valid param names defined by us
+	res.send(roll(6, 2, 1));
+	
+});
+app.get('/app/roll/:sides/', (req, res, next) => {
+	 // return JSON for default num of rolls/dice with whatever num sides
+	 res.send(roll(req.params.sides, 2, 1));
+});
+app.get('app/roll/:sides/:dice/', (req, res, next) => {
+	//return json w/ default num/rolls w/ whatever num sides/dice
+	res.send(roll(req.params.sides, req.params.dice, 1));
+});
+app.get('app/roll/:sides/:dice/:rolls/', (req, res, next) => {
+	//return json w/ given params for roll-dice
+	res.send(roll(req.params.sides, req.params.dice, req.params.rolls));
+});
 
 app.listen(port, () => {
 	console.log("Server listening on port " + port);
-})
+});
