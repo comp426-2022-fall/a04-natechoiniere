1f27152b070d0be69c8695accb8dda04ce67ef16
Nate Choiniere <choiniere@unc.edu>
2022-11-30 07:43:46 -0500

    a04

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = parsedArgs.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('.*', (req, res) => {
+app.use('*', (req, res) => {
 	res.status(200).send('404 NOT FOUND');
 });
 app.get('/app/', (req, res, next) => {
@@ -28,15 +28,15 @@ app.post('/app/roll/', (req, res) => {
 });
 app.get('/app/roll/:sides/', (req, res) => {
 	 // return JSON for default num of rolls/dice with whatever num sides
-	 res.send(roll(req.params.sides, 2, 1));
+	 res.send(roll(int(req.params.sides), 2, 1));
 });
 app.get('app/roll/:sides/:dice/', (req, res) => {
 	//return json w/ default num/rolls w/ whatever num sides/dice
-	res.send(roll(req.params.sides, req.params.dice, 1));
+	res.send(roll(int(req.params.sidesi), int(req.params.dice), 1));
 });
 app.get('app/roll/:sides/:dice/:rolls/', (req, res) => {
 	//return json w/ given params for roll-dice
-	res.send(roll(req.params.sides, req.params.dice, req.params.rolls));
+	res.send(roll(int(req.params.sides), int(req.params.dice), int(req.params.rolls)));
 });
 
 app.listen(port, () => {
