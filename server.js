3b48042225ebb2d4954db85ac165e63d2abf5bc5
Nate Choiniere <choiniere@unc.edu>
2022-11-30 06:41:17 -0500

    a04

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get('/', (req, res, next) => { // endpoint is the first arg
 	res.status(404).send('404 NOT FOUND');
 	
 });
+app.use((req, res) => {
+	res.status(404).send('404 NOT FOUND');
+}
 app.get('/app/', (req, res, next) => {
 	res.status(200).send('200 OK');
 	
