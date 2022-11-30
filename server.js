82d4d713d511bf11b095ce25dc92b3cf7d3e6e53
Nate Choiniere <choiniere@unc.edu>
2022-11-30 07:40:35 -0500

    a04

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const port = parsedArgs.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use((req, res) => {
-	res.status(404).send('404 NOT FOUND');
+app.use('.*', (req, res) => {
+	res.status(200).send('404 NOT FOUND');
 });
 app.get('/app/', (req, res, next) => {
 	res.status(200).send('200 OK');
